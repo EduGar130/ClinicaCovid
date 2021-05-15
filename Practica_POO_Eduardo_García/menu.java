f687881b97bdeda770c22cea39f66ef33851775e
EduGar130 <53255990+EduGar130@users.noreply.github.com>
2021-05-15 13:07:51 +0200

    Add files via upload

---
+++ Practica_POO_Eduardo_García/menu.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class menu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+
+public class menu {
+    public static void main(String[] args) {
+    // instance variables - replace the example below with your own
+    int seleccion;
+
+    /**
+     * Constructor for objects of class menu
+     */
+        System.out.println("HOLA"); //
+    }
+}
